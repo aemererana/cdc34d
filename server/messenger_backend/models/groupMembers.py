9331c6b340342ed00ce0178e78479eac1f1ac8e0
Aemer <56653799+aemererana@users.noreply.github.com>
2022-03-30 21:41:18 -0600

    Uniform Formatting

--- server/messenger_backend/models/groupMembers.py
+++ server/messenger_backend/models/groupMembers.py
@@ -5,9 +5,6 @@ from .user import User
 from .group import Group
 
 class GroupMembers(utils.CustomModel):
-    userId = models.ForeignKey(
-        User, 
-        on_delete=models.CASCADE
-    )
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
     belongingGroupId = models.ForeignKey(Group, on_delete=models.CASCADE)
     joinedOn = models.DateTimeField(auto_now_add=True)
